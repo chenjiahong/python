0a38d773f175f518ba943a151d403586e2cbfcf7
chenjiahong <jychenjiahong@gmail.com>
2015-03-28 22:52:15 +0800

    change price to low and high price, add some stock number

--- watchHKStock.py
+++ watchHKStock.py
@@ -12,36 +12,39 @@ uname = platform.uname();
 print "System and machine: %s, Python version: %s"%(uname, pythonVersion);
 
 i = 0 
-destPrice = 11.1000
+highPrice = [11.50, 3.6, 3.2]
+lowPrice = [10.80, 3.2, 2.6]
+stockNumList = [992, 543, 1027]
 endTagStrList = [
-			'</span></span> <span class="up_g time_rtq_content">',
+			#'</span></span> <span class="up_g time_rtq_content">',
 			'</span></span></span></span></span></span>',
 			]
 stockUrlList = [
 			'http://hk.finance.yahoo.com/q?s=0992.HK%2C+&ql=1', #雅虎香港
-			'http://www.aastocks.com/tc/ltp/rtquote.aspx?symbol=992', #阿斯达克
+			'http://www.aastocks.com/tc/ltp/rtquote.aspx?symbol=%d', #阿斯达克
 			]
-sourceIdx = 1
+sourceIdx = 2
+targetUrl = stockUrlList[1] % (stockNumList[sourceIdx])
 while 1:
 	i = i + 1
 	currtime = time.localtime()
 	timeStr = time.strftime("%d %b %Y %H:%M:%S", currtime)
 	#print currtime.tm_hour
 	try:
-		request = urllib2.Request(stockUrlList[sourceIdx], unverifiable=True)
+		request = urllib2.Request(targetUrl, unverifiable=True)
 		response = urllib2.urlopen(request, timeout = 30) 
 		htmlContent = response.read()
 	except:
 		print '%d. %s Some error/exception occurred. Let me try again' %(i, timeStr)
 		continue
 	else:
-		endPos = htmlContent.find(endTagStrList[sourceIdx])
+		endPos = htmlContent.find(endTagStrList[0])
 		beginPos = htmlContent.find('>', endPos - 10) + 1
 		#print "%d, %d" %(beginPos, endPos)
 		priceStr = htmlContent[beginPos : endPos]
-		print "%d. %s HK992 current price: %s" %(i, timeStr, priceStr)
+		print "%d. %s HK%d current price: %s" %(i, timeStr, stockNumList[sourceIdx], priceStr)
 		currPrice = string.atof(priceStr)
-		if currPrice >= destPrice:
+		if currPrice >= highPrice[sourceIdx] or currPrice <= lowPrice[sourceIdx]:
 			win32api.MessageBox(0, priceStr, "current price", win32con.MB_SETFOREGROUND)
 		#finishTime = time.localtime()
 		#print "cost time: %d s" %(finishTime.tm_sec - currtime.tm_sec)
